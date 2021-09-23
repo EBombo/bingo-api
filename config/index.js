38dfe14f81046dd96a1f3240f49bb6a865b5a510
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-09-23 15:37:36 -0500

    add env [prod & dev]

--- config/index.js
+++ config/index.js
@@ -1,13 +1,25 @@
 const admin = require("firebase-admin");
-const url = require("url");
 const configJson = require("./config");
+const url = require("url");
+const serviceAccountEventsDev = require("./ebombo-events-dev-firebase-adminsdk-brntw-ad09602471.json");
 
-admin.initializeApp();
+const config =
+    process.env.NODE_ENV === "production"
+        ? configJson.production
+        : configJson.development;
+
+process.env.NODE_ENV === "production"
+    ? admin.initializeApp()
+    : admin.initializeApp({
+      credential: admin.credential.cert(serviceAccountEventsDev),
+      databaseURL: config.firebase.databaseURL,
+    });
+
 const adminFirestore = admin.firestore;
 const firestore = admin.firestore();
 const auth = admin.auth();
-const projectId = process.env.GCLOUD_PROJECT;
-const currentEnvironment = projectId.includes("-dev") ? "dev" : "production";
+const currentEnvironment = process.env.NODE_ENV;
+const version = "0.0.1";
 
 try {
   firestore.settings({ ignoreUndefinedProperties: true });
@@ -15,17 +27,14 @@ try {
   console.error("ignoreUndefinedProperties", error);
 }
 
-const config = projectId.includes("-dev")
-  ? configJson.devConfig
-  : configJson.productionConfig;
-
 const hostname = (req) => url.parse(req.headers.origin).hostname;
 
 module.exports = {
-  currentEnvironment,
   adminFirestore,
+  currentEnvironment,
   firestore,
   hostname,
-  config,
   auth,
+  config,
+  version,
 };
