2b807f54ac6077931e477b6ff8937b1cb8754995
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-10-14 11:25:42 -0500

    ....

--- api/lobbies/put/expireLobbies.js
+++ api/lobbies/put/expireLobbies.js
@@ -3,16 +3,20 @@ const {
   // updateLobbiesIsClosedField,
 } = require("../../../collections/lobbies");
 const logger = require("../../../utils/logger");
+const moment = require("moment")
 
-const LAST_HOURS_TO_RETRIEVE = -5;
+//const LAST_HOURS_TO_RETRIEVE = -5;
+const LAST_HOURS_TO_RETRIEVE = 5;
 
 exports.expireLobbies = async (req, res, next) => {
   try {
     logger.log("expireLobbies ->", req.params, req.query, req.body);
 
+    //const lastHour=moment().subtract(LAST_HOURS_TO_RETRIEVE,"hours").toDate()
+
     const lobbiesRef = await fetchLobbiesNotClosedAndCreatedLastHoursRef(LAST_HOURS_TO_RETRIEVE);
 
-    if (!lobbiesRef) {
+    if (!lobbiesRef?.length) {
       logger.log("lobbiesRef is empty:", lobbiesRef);
       return res.send({ success: false });
     }
@@ -20,7 +24,7 @@ exports.expireLobbies = async (req, res, next) => {
     logger.log("number of lobbies retrieved", lobbiesRef.docs.length);
     logger.log("all lobbies's timestamp:", lobbies.docs.map(d => d.data().createAt.toString()).join(', '));
 
-    // const result = await updateLobbiesIsClosedField(lobbiesRef, true)
+    // const result = await updateLobbiesIsClosedField(lobbiesRef, {isClosed:true}})
     // logger.log("result", result);
     return res.send({ success: true });
   } catch (error) {
--- collections/lobbies/index.js
+++ collections/lobbies/index.js
@@ -1,40 +1,56 @@
-const { firestore } = require("../../config");
-const { currentDatetimeWithOffsetHours, snapshotToArray } = require("../../utils");
+const {firestore} = require("../../config");
+const {currentDatetimeWithOffsetHours} = require("../../utils");
 const chunk = require("lodash/chunk");
 
-lobbiesRef = firestore.collection(`lobbies`)
-
 const fetchLobbiesNotClosedAndCreatedLastHoursRef = async (hoursFromNow) => {
-  const lobbiesQuery = await firestore
-    .collection("lobbies")
-    .where("isClosed", "==", false)
-    .where("createAt", ">=", currentDatetimeWithOffsetHours(hoursFromNow))
-    .get();
+    const lobbiesQuery = await firestore
+        .collection("lobbies")
+        .where("isClosed", "==", false)
+        .where("createAt", ">=", currentDatetimeWithOffsetHours(hoursFromNow))
+        .get();
 
-  return lobbiesQuery.docs.map(doc => doc.ref);
-      // CASE 2
-  // return snapshotToArray(lobbiesQuery);
+    return lobbiesQuery.docs.map(doc => doc.ref);
+    // CASE 2
+    // return snapshotToArray(lobbiesQuery);
 };
 
 const BATCH_MAX_LIMIT_TRANSACTION = 500;
-const updateLobbiesIsClosedField = async (lobbiesRefs, newIsClosedValue = true) => {
-  const batch = firestore.batch();
-  const lobbiesRefsChunks = chunk(lobbiesRefs, size=BATCH_MAX_LIMIT_TRANSACTION);
 
-  let writeResults = [];
+const updateLobbiesIsClosedField = async (lobbiesRefs, props) => {
 
-  for (let i = 0; i < lobbiesRefsChunks.length; i++) {
-    const lobbiesRefsChunk = lobbiesRefsChunks[i];
-    lobbiesRefsChunk.forEach((lobbyRef) => {
-      // CASE 2
-      // const lobbyRef = firestore.doc(`lobbies/${lobby.id}`);
-      batch.update(lobbyRef, 'isClosed', newIsClosedValue);
+    const lobbiesRefsChunks = chunk(lobbiesRefs, BATCH_MAX_LIMIT_TRANSACTION);
+
+    const promises = lobbiesRefsChunks.map(async lobbyChunk => {
+
+        const batch = firestore.batch();
+
+        lobbyChunk.forEach(lobby => {
+                const lobbyRef = firestore.doc(`lobbies/${lobby.id}`);
+                batch.update(lobbyRef, 'isClosed', props);
+            }
+        );
+
+        await batch.commit()
     });
 
-    writeResults.push(await batch.commit()); 
-  }
+    await Promise.all(promises)
 
-  return writeResults;
+    /*
+    let writeResults = [];
+
+    for (let i = 0; i < lobbiesRefsChunks.length; i++) {
+      const lobbiesRefsChunk = lobbiesRefsChunks[i];
+      lobbiesRefsChunk.forEach((lobbyRef) => {
+        // CASE 2
+        // const lobbyRef = firestore.doc(`lobbies/${lobby.id}`);
+        batch.update(lobbyRef, 'isClosed', newIsClosedValue);
+      });
+
+      writeResults.push(await batch.commit());
+    }
+
+    return writeResults;
+     */
 }
 
-module.exports = { fetchLobbiesNotClosedAndCreatedLastHoursRef, updateLobbiesIsClosedField };
+module.exports = {fetchLobbiesNotClosedAndCreatedLastHoursRef, updateLobbiesIsClosedField};
