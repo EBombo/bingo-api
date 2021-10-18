b62d1ce3760f5a42d8f7482eee85f8aaf5f98c7b
Pablo <tech@ebombo.com>
2021-10-18 00:01:53 +0000

    prevent crash using ?

--- api/lobbies/put/expireLobbies.js
+++ api/lobbies/put/expireLobbies.js
@@ -1,33 +1,34 @@
 const {
-  fetchLobbiesNotClosedAndCreatedLastHoursRef,
-  updateLobbiesIsClosedField,
+    fetchLobbiesNotClosedAndCreatedLastHoursRef,
+    updateLobbiesIsClosedField,
 } = require("../../../collections/lobbies");
 const logger = require("../../../utils/logger");
 const { currentDatetimeWithOffsetHours } = require("../../../utils");
+const get = require("lodash/get");
 
 const LAST_HOURS_TO_RETRIEVE = 5;
 
 exports.expireLobbies = async (req, res, next) => {
-  try {
-    logger.log("expireLobbies ->", req.params, req.query, req.body);
+    try {
+        logger.log("expireLobbies ->", req.params, req.query, req.body);
 
-    const lastHours = currentDatetimeWithOffsetHours(LAST_HOURS_TO_RETRIEVE);
+        const lastHours = currentDatetimeWithOffsetHours(LAST_HOURS_TO_RETRIEVE);
 
-    const lobbiesRef = await fetchLobbiesNotClosedAndCreatedLastHoursRef(lastHours);
+        const lobbiesRef = await fetchLobbiesNotClosedAndCreatedLastHoursRef(lastHours);
 
-    if (!lobbiesRef?.length) {
-      logger.log("lobbiesRef is empty:", lobbiesRef);
-      return res.send({ success: false });
+        if (!get(lobbiesRef, "length")) {
+            logger.log("lobbiesRef is empty:", lobbiesRef);
+            return res.send({ success: false });
+        }
+
+        const result = await updateLobbiesIsClosedField(lobbiesRef, { isClosed: true });
+
+        // TODO: Consider remove the logger.
+        logger.log("result", result);
+
+        return res.send({ success: true });
+    } catch (error) {
+        logger.error(error);
+        next(error);
     }
-
-    const result = await updateLobbiesIsClosedField(lobbiesRef, { isClosed: true });
-
-    // TODO: Consider remove the logger.
-    logger.log("result", result);
-
-    return res.send({ success: true });
-  } catch (error) {
-    logger.error(error);
-    next(error);
-  }
 };
